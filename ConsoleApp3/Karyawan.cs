fcd28fc3f4bcd3f8144ce5bd4c9a9f120d1a09aa
Feysha Rahadian Syah <101683544+alstroemeriaP@users.noreply.github.com>
2022-04-20 09:46:14 +0700

    -

---
+++ ConsoleApp3/Karyawan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4467
+{
+    class Karyawan
+    {
+        public Karyawan()
+        {
+            
+        }
+
+        public Karyawan (string Nama, string Nik, int GajiBulanan)
+        {
+            Nama = Nama;
+            Nik = Nik;
+
+            if (GajiBulanan > 0)
+            { GajiBulanan = 0; }
+            else { GajiBulanan = GajiBulanan; }
+        }
+        public string Nama { get; set; }
+        public string Nik { get; set; }
+        public int GajiBulanan { get; set; }
+    }
+}
